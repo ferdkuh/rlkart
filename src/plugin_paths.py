843b57cc95b87f25cae35959df3fb8673a0f83ca
Jones <jciver@safetymail.info>
2017-06-14 18:04:41 +0200

    lala

--- src/plugin_paths.py
+++
@@ -1,5 +0,0 @@
-dll_path = r"k:\temp\dll\mupen64plus.dll"
-input_plugin_path = r"k:\temp\dll\mupen64plus-input-sdl.dll"
-video_plugin_path = r"k:\temp\dll\mupen64plus-video-rice.dll"
-#video_plugin_path = r"k:\temp\dll\mupen64plus-video-glide64mk2.dll"
-rsp_plugin_path = r"k:\temp\dll\mupen64plus-rsp-hle.dll"
